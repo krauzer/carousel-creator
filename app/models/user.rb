1dbab9ebc9386bf6bee8858388fe75393d61e499
Dylan Krause <dykrause@gmail.com>
2015-01-04 15:06:42 -0500

    Remove default has_secure_password method from user model and go with basic inclusion of BCrypt module -define methods that read and create passwords for the User based on BCrypt's Password Class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  # Remember to create a migration!
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
