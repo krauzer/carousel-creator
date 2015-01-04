4119ce2b049782cc2213dece83dd00d2c0918d45
Dylan Krause <dykrause@gmail.com>
2015-01-04 13:47:35 -0500

    Remove skeleton migration for users and add new one

--- db/migrate/20150104133216_create_users.rb
+++ db/migrate/20150104133216_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :users do |table|
+      t.string :email
       t.string :name
-      t.string :password_digest
     end
   end
 end
