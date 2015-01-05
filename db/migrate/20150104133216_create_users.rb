5b4ab9a57202ce104619e5f564da7af9c8986bce
Dylan Krause <dykrause@gmail.com>
2015-01-05 15:49:15 -0500

    Add timestamps to user table and change variable name to t

--- db/migrate/20150104133216_create_users.rb
+++ db/migrate/20150104133216_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |table|
+    create_table :users do |t|
       t.string :email
       t.string :name
+
+      t.timestamps
     end
   end
 end
