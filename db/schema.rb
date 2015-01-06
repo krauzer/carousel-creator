8acb8d47c161a10163110fe1065d9ad082cca21e
Dylan Krause <dykrause@gmail.com>
2015-01-05 23:56:45 -0500

    Update schema with not null constraints

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105153931) do
+ActiveRecord::Schema.define(version: 20150105233342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
-    t.string   "image_id"
+    t.string   "image_id",   null: false
     t.string   "image_name"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 20150105153931) do
   add_index "photos", ["user_id"], name: "index_photos_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
+    t.string   "email",         null: false
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_hash"
+    t.string   "password_hash", null: false
   end
 
 end
