99f7d25c2d65393a1019555491bf7134697df1ec
Dylan Krause <dykrause@gmail.com>
2015-01-06 19:05:37 -0500

    Set Refile store and cache backends to be filesystem backend if app is in development

--- config/refile_initializer.rb
+++ config/refile_initializer.rb
@@ -2,3 +2,8 @@
 ActiveSupport.on_load :active_record do
         require "refile/attachment/active_record"
 end
+
+if Sinatra::Application.development?
+  Refile.store ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "store"))
+  Refile.cache ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "cache"))
+end
