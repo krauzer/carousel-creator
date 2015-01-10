639af9bc9c4637312531a5f10ff2a6f4b72f920e
Dylan Krause <dykrause@gmail.com>
2015-01-09 21:23:47 -0500

    Add test on user model that checks for password presence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe User, type: :model do
+
   it "is invalid without an email address" do
     user = build(:user, email: nil)
     expect(user).to be_invalid
   end
+
+  it "is invalid without a password" do
+    user = build(:user, password_hash: nil)
+    expect(user).to be_invalid
+  end
+
 end
