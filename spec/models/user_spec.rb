af54aac886cb39024a17b2a8106ab595066c3723
Dylan Krause <dykrause@gmail.com>
2015-01-10 02:09:34 -0500

    Add test to user that checks for assertion on setting password to nil

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,8 @@ describe User, type: :model do
     expect {user.password_hash = nil}.to change{user.valid?}.from(true).to(false)
   end
 
+  it "raises an error when passed a nil password" do
+    user = build(:user)
+    expect {user.password = nil }.to raise_error
+  end
 end
