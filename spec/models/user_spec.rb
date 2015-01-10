4babbe93de23cd1d9fa4f8eecdca8a8fdf293125
Dylan Krause <dykrause@gmail.com>
2015-01-10 02:05:55 -0500

    Change user specs to better fit validation checks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe User, type: :model do
 
   it "is invalid without an email address" do
-    user = build(:user, email: nil)
-    expect(user).to be_invalid
+  user = build(:user)
+   expect {user.email = nil}.to change{user.valid?}.from(true).to(false)
   end
 
   it "is invalid without a password" do
-    user = build(:user, password_hash: nil)
-    expect(user).to be_invalid
+    user = build(:user)
+    expect {user.password_hash = nil}.to change{user.valid?}.from(true).to(false)
   end
 
 end
