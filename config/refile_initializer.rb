092f1ee4076df0f8ac104f907d29b5f981d07ec4
Dylan Krause <dykrause@gmail.com>
2015-01-12 13:02:40 -0500

    Load S3 credentials from environment variables

--- config/refile_initializer.rb
+++ config/refile_initializer.rb
@@ -3,6 +3,12 @@ ActiveSupport.on_load :active_record do
         require "refile/attachment/active_record"
 end
 
+aws = {
+  access_key_id: ENV['S3_ACCESS_KEY_ID'],
+  secret_access_key: ENV['S3_SECRET_KEY'],
+  bucket: ENV['S3_BUCKET']
+}
+
 if Sinatra::Application.development?
   Refile.store ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "store"))
   Refile.cache ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "cache"))
