177662508b5b9aba806433139039981880183643
Dylan Krause <dykrause@gmail.com>
2015-01-12 18:50:16 -0500

    Write outline for photo spec tests

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -2,5 +2,48 @@ require 'spec_helper'
 
 describe Photo, type: :model do
 
+  describe "#image" do
+
+    context "when attaching an image" do
+
+      it "sets the image_id property" do
+      end
+
+      it "sets the image_id property" do
+      end
+
+      it "sets the image_filename property" do
+      end
+
+      it "sets the image_size property" do
+      end
+
+      it "sets the image_content_type property" do
+      end
+
+      it "sends it to the cache backend" do
+      end
+    end
+
+    context "when attaching a file that is not an image" do
+
+      it "raises an assertion error" do
+      end
+    end
   end
+
+  describe "#save" do
+
+    context "when saving with an attached image" do
+
+      it "sends the attached image to the store backend" do
+      end
+    end
+
+      context "when saving without an attached image" do
+
+        it "raises a validation error" do
+        end
+      end
+    end
 end
