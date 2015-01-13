6af42c8fc648c5a8da1cc0df377498ea1dabdb2f
Dylan Krause <dykrause@gmail.com>
2015-01-13 13:57:46 -0500

    Add specs for attaching an image to the photo model

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,21 +1,35 @@
 require 'spec_helper'
 
 describe Photo, type: :model do
+  let(:photo) {Photo.new}
 
   describe "#image" do
 
     context "when attaching an image" do
+      before(:example) do
+        File.open("spec/fixtures/sample_image.jpg", "rb") do |file|
+          photo.image = file
+        end
+      end
+      after(:example) {FileUtils.rm_rf(Dir.glob('spec/tmp/uploads/cache/*'))}
 
       it "sets the image_filename property" do
+        expect(photo.image_filename).to eq("sample_image.jpg")
       end
 
       it "sets the image_size property" do
+        expect(photo.image_size).to eq(104870)
       end
 
       it "sets the image_content_type property" do
+        expect(photo.image_content_type).to eq("image/jpeg")
       end
 
       it "sends it to the cache backend" do
+        cache_paths = Dir["spec/tmp/uploads/cache/*"]
+        matches = 0
+        cache_paths.each {|path| matches += 1 if path.include? photo.image.id.to_s}
+        expect(matches).to eq(1)
       end
     end
 
