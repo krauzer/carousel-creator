e30595f44c5601aa4fae33c8d88005f11d4dd9d8
Dylan Krause <dykrause@gmail.com>
2015-01-13 11:46:57 -0500

    Set the test backend to the file system and the production to S3 -Filesystem more testable

--- config/refile_initializer.rb
+++ config/refile_initializer.rb
@@ -9,9 +9,12 @@ aws = {
   bucket: ENV['S3_BUCKET']
 }
 
-if Sinatra::Application.development?
+if Sinatra::Application.development? || test?
   Refile.store ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "store"))
   Refile.cache ||= Refile::Backend::FileSystem.new(APP_ROOT.join("tmp", "uploads", "cache"))
+else
+  Refile.store ||= Refile::Backend::S3.new(prefix: "store", **aws)
+  Refile.cache ||= Refile::Backend::S3.new(prefix: "cache", **aws )
 end
 
 Refile.app = Refile::App.new
