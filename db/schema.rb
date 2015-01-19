c8493c37e30f74818f6ce557f7c02b797c2831d1
Dylan Krause <dykrause@gmail.com>
2015-01-19 15:55:13 -0500

    Change parameter_key attribute type on user model to be a string rather than an integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119141538) do
+ActiveRecord::Schema.define(version: 20150119155116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150119141538) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_hash", null: false
-    t.integer  "parameter_key"
+    t.string   "parameter_key"
   end
 
 end
