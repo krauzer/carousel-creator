f755c4f205e30ada4a7b8e808b6dd11e2f2656d6
Dylan Krause <dykrause@gmail.com>
2015-01-19 16:03:03 -0500

    Add creation post route for users -define build and save methods for users that are called in route -check for valid user parameters in seperate method -redirect to user page if created

---
+++ app/routes/user.rb
@@ -0,0 +1,37 @@
+
+post '/users' do
+  build_user
+  save_user or erb :'/auth/signup'
+end
+
+get '/users/:parameter_key' do
+end
+
+
+def build_user
+  user = validate_user_params
+  @user ||= User.new user
+end
+
+def save_user
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.parameter_key}"
+  end
+end
+
+def validate_user_params
+  param :name,            String
+  param :email,           String, blank: false
+  param :password,        String, blank: false
+  param :confirmation,    String
+  unless params[:name] == ""
+    name = params[:name]
+  else
+    name = params[:email]
+  end
+  user =  {name: name,
+          email: params[:email],
+          password: params[:password]
+        }
+end
