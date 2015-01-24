c9960e64b6575f46ab47c7cd3cc07832ace11d74
Dylan Krause <dykrause@gmail.com>
2015-01-24 17:26:34 -0500

    Move user methods in the user helper file

--- app/routes/user.rb
+++ app/routes/user.rb
@@ -12,32 +12,3 @@ get '/users/:parameter_key' do |param_key|
     no_user
   end
 end
-
-
-def build_user
-  user = validate_user_params
-  @user ||= User.new user
-end
-
-def save_user
-  if @user.save
-    session[:user_id] = @user.id
-    redirect "/users/#{@user.parameter_key}"
-  end
-end
-
-def validate_user_params
-  param :name,            String
-  param :email,           String, blank: false
-  param :password,        String, blank: false
-  param :confirmation,    String
-  unless params[:name] == ""
-    name = params[:name]
-  else
-    name = params[:email]
-  end
-  user =  {name: name,
-          email: params[:email],
-          password: params[:password]
-        }
-end
